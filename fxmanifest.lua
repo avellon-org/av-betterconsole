376d376b2a3e47b43da5a9ec498c97efc1236b9f
MTL | Alex <92187331+divide29@users.noreply.github.com>
2023-03-15 00:05:36 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'avellon'
+description 'better be not fucking around with the laziness of developers'
+version '1.0.0'
+
+shared_scripts {
+	'config.lua',
+}
+
+client_scripts {
+	'client.lua'
+}
+server_scripts {
+	'server.lua'
+}
+
+lua54 'yes'
